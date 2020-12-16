0595796c60590259589afbce6b26f361ab7c1206
Victor Akpan <v.akpan@live.com>
2020-12-16 20:49:49 +0400

    added local storage

--- Beam/Beam.Tests/IndexPagesTests.cs
+++ Beam/Beam.Tests/IndexPagesTests.cs
@@ -1,7 +1,9 @@
 ﻿using Beam.Client.Pages;
 using Beam.Tests.Auth;
 using Bunit;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.Extensions.DependencyInjection;
 using Xunit;
 
 namespace Beam.Tests
@@ -15,6 +17,7 @@ namespace Beam.Tests
                 .CreateAuthenticationState("TestUser"));
 
             var wrapper = RenderComponent<CascadingAuthenticationState>(p => p.AddChildContent<Index>());
+
             // Arrange
             var cut = wrapper.FindComponent<Index>();
 
@@ -22,6 +25,23 @@ namespace Beam.Tests
             Assert.Contains("Select a Frequency, or create a new one", cut.Markup);
             cut.MarkupMatches(@"<h2 diff:ignore></h2>
                                         <p diff:ignore></p>");
+        }
+
+        [Fact]
+        public void IndexShowsLoginLinkWhenUnAuthorized()
+        {
+            Services.AddAuthenticationServices(TestAuthenticationStateProvider.CreateUnauthenticationState(),
+                AuthorizationResult.Failed());
+
+            Services.AddScoped<MockNavigationManager, MockNavigationManager>();
+            var wrapper = RenderComponent<CascadingAuthenticationState>(p => p.AddChildContent<Index>());
+
+            // Arrange
+            var cut = wrapper.FindComponent<Index>();
+
+            Assert.DoesNotContain("Select a Frequency, or create a new one", cut.Markup);
+
+            cut.Find("a").MarkupMatches(@"<a href=""/login"">Log In</a>");
         }
     }
 }
---
+++ Beam/Beam.Tests/TestLocalStorage.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading.Tasks;
+using Blazored.LocalStorage;
+using System.Collections.Generic;
+
+namespace Beam.Tests
+{
+    public class TestLocalStorage : ILocalStorageService
+    {
+        private Dictionary<string, object> dictionary = new Dictionary<string, object>();
+        public event EventHandler<ChangingEventArgs> Changing;
+        public event EventHandler<ChangedEventArgs> Changed;
+
+        public ValueTask ClearAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<bool> ContainKeyAsync(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<string> GetItemAsStringAsync(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<T> GetItemAsync<T>(string key)
+        {
+            if (dictionary.ContainsKey(key))
+                return ValueTask.FromResult((T)dictionary[key]);
+            else
+                return ValueTask.FromResult(default(T));
+        }
+
+        public ValueTask<string> KeyAsync(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<int> LengthAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask RemoveItemAsync(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask SetItemAsync<T>(string key, T data)
+        {
+            Changing?.Invoke(this, new ChangingEventArgs() { Key = key, NewValue = data });
+            dictionary[key] = data;
+            Changed?.Invoke(this, new ChangedEventArgs() { Key = key, NewValue = data });
+            return ValueTask.CompletedTask;
+        }
+    }
+}
